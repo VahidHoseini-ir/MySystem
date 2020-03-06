601b1ae134e86c2dba3ed71f3cc3c36f31b2ea63
Vahid Hoseini <vahid09390292608@gmail.com>
2020-03-07 03:23:36 +0330

    Add files via upload

---
+++ conf.java
@@ -0,0 +1,38 @@
+package ir.vahidhoseini.callrecorder;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.onesignal.OSInAppMessageAction;
+import com.onesignal.OSNotificationAction;
+import com.onesignal.OSNotificationOpenResult;
+import com.onesignal.OSPermissionSubscriptionState;
+import com.onesignal.OneSignal;
+
+import org.json.JSONObject;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+public class conf extends Application {
+    public static Context context;
+    public static Activity currentActivity;
+    public static boolean ActivatingExAds = false;
+    public static boolean ActivatingMyAds = false;
+    public static boolean ActivatingGlobalAd = false;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("yekan.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
+    }
+
+}
